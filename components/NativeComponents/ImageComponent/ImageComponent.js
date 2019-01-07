b0fe34996805957437c8573dae6d9ccf25a198bd
Mario Lo <kinfonglo@overstock.com>
2019-01-07 15:39:52 -0700

    lint fixes, update to pure component when possible

--- components/NativeComponents/ImageComponent/ImageComponent.js
+++ components/NativeComponents/ImageComponent/ImageComponent.js
@@ -1,24 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { AppRegistry, Image } from 'react-native';
 
-class ImageComponent extends Component {
-    render() {
-        const pic = {
-            uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-        };
+const ImageComponent = () => {
+    const pic = {
+        uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
 
-        const style = {
-            height: 110,
-            width: 193
-        };
+    const style = {
+        height: 110,
+        width: 193
+    };
 
-        return (
-            <Image
-                source={pic}
-                style={style} />
-        );
-    }
-}
+    return (
+        <Image source={pic} style={style} />
+    );
+};
 
 AppRegistry.registerComponent('AwesomeProject', () => ImageComponent);
 export default ImageComponent;
--- components/Size/FixedDimensions.js
+++ components/Size/FixedDimensions.js
@@ -1,17 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { AppRegistry, View } from 'react-native';
 
-export default class FixedDimensions extends Component {
-    render() {
-        return (
-            <View style={{ flex: 1, flexDirection: 'row' }}>
-                <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
-                <View style={{ width: 100, height: 100, backgroundColor: 'skyblue' }} />
-                <View style={{ width: 150, height: 150, backgroundColor: 'steelblue' }} />
-            </View>
-        );
-    }
-}
+const FixedDimensions = () => (
+    <View style={{ flex: 1, flexDirection: 'row' }}>
+        <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
+        <View style={{ width: 100, height: 100, backgroundColor: 'skyblue' }} />
+        <View style={{ width: 150, height: 150, backgroundColor: 'steelblue' }} />
+    </View>
+);
 
 // skip this line if using Create React Native App
 AppRegistry.registerComponent('AwesomeProject', () => FixedDimensions);
+export default FixedDimensions;

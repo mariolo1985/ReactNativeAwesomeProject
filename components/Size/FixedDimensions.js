34770b4beeabd2c16dd35e66dd0bb7f615ac4821
Mario Lo <kinfonglo@overstock.com>
2019-01-07 16:45:55 -0700

    set as column

--- components/Size/FixedDimensions.js
+++ components/Size/FixedDimensions.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { AppRegistry, View } from 'react-native';
 
 const FixedDimensions = () => (
-    <View style={{ flex: 1, flexDirection: 'row' }}>
+    <View style={{ flex: 1, flexDirection: 'column' }}>
         <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
         <View style={{ width: 100, height: 100, backgroundColor: 'skyblue' }} />
         <View style={{ width: 150, height: 150, backgroundColor: 'steelblue' }} />

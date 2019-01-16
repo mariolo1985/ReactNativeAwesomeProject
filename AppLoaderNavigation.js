ba14545e8dfdc4b949f9b322a9ee1859908ea58c
Mario Lo <kinfonglo@overstock.com>
2019-01-16 16:06:52 -0700

    app loader helpers

---
+++ AppLoaderNavigation.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import AppNavigator from './components/Screens/AppNavigator';
+
+const AppLoaderNavigation = () => (
+    <AppNavigator />
+);
+export default AppLoaderNavigation;
